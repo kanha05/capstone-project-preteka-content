6a85fe87d8e90c1a2da8ef59dad8e671ab1f2c67
kimlang17 <118195132+kimlang17@users.noreply.github.com>
2023-04-27 12:22:27 +0700

    add font and dark mode

--- preteka-content/resources/views/detail.blade.php
+++ preteka-content/resources/views/detail.blade.php
@@ -17,7 +17,7 @@
       <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"></path>
       </svg>
     </div>
-    <h1 class="text-2xl font-bold text-slate-950 grid place-content-center">
+    <h1 class="text-2xl font-bold text-slate-950 grid place-content-center dark:text-white">
     Begin your Tai Chi journey: Traditional 
     </h1>
     <div class="p-8">
