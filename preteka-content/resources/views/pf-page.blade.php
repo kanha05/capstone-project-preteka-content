274df0b4bed2961f46177e3b0d641a17a8598e1d
kimlang17 <118195132+kimlang17@users.noreply.github.com>
2023-05-02 15:36:22 +0700

    add layout to all page, modify footer

--- preteka-content/resources/views/pf-page.blade.php
+++ preteka-content/resources/views/pf-page.blade.php
@@ -1,17 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://kit.fontawesome.com/f930ed931d.js" crossorigin="anonymous"></script>
-
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
-
 <body class="font-poppins">
     <main>
         <section id="profile">
@@ -776,5 +772,4 @@
     }
     </script>
 </body>
-
 </html>
