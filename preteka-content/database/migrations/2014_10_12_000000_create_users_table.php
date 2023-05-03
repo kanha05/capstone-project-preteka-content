081fc023c212d8f1bb96c7936e2b49bcc89ab4e8
kanha05 <kanhasongeam2309@gmail.com>
2023-05-03 12:12:52 +0700

    create_database_table

--- preteka-content/database/migrations/2014_10_12_000000_create_users_table.php
+++ preteka-content/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,24 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
+
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string("password", 255);
+            $table->string("phone_number", 255)->nullable();
+            $table->string("pf_bio", 255)->nullable();
+            $table->string("work", 255)->nullable();
+            $table->string("edu", 255)->nullable();
+            $table->string("facebook", 255)->nullable();
+            $table->string("instagram", 255)->nullable();
+            $table->string("telegram", 255)->nullable();
+            $table->string("linkedin", 255)->nullable();
+            $table->string("pf-image", 255)->nullable();
+            $table->string("cover-image", 255)->nullable();
+            $table->biginteger('role');
+            $table->date('dob')->date_format('d.m.Y')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
